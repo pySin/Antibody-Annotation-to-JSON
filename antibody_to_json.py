48d69adbdbd7fb7d9dfc0656a73c05182d2bfd75
Sinan Mehmedov <sinanat@abv.bg>
2024-11-11 17:44:39 +0000

    Save Code

--- antibody_to_json.py
+++ antibody_to_json.py
@@ -9,13 +9,28 @@ class AntibodyTxtJSON:
     @staticmethod
     def txt_to_dict(filename):
         hanging_key = None
+        accumulate = []
         with open(filename, "r") as f:
             for line in f.readlines():
                 if len(line) > 1:
                     split_line = line.strip().split(": ", 1)
-                    if len(split_line) == 1:
+                    if len(split_line) == 1 and ":" in split_line[0]:
                         hanging_key = split_line[0]
-                    print(split_line)
+                        print(f"Inner key: {hanging_key}")
+                    elif len(split_line) == 1:
+                        accumulate.append(split_line[0])
+                        print(f"Accumulate: {accumulate}")
+                    else:
+                        if hanging_key is not None:
+                            print([hanging_key, accumulate])
+                            hanging_key = None
+                            accumulate = []
+                        else:
+                            print(split_line)
+            if hanging_key is not None:
+                print([hanging_key, accumulate])
+                hanging_key = None
+                accumulate = []
 
 
 def main():
