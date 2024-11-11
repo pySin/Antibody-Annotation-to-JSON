13e568de1303c2b335158822851842785309057c
Sinan Mehmedov <sinanat@abv.bg>
2024-11-11 16:31:03 +0000

    Save Code

--- antibody_to_json.py
+++ antibody_to_json.py
@@ -1,3 +1,17 @@
 #  Populate JSON file from text data
 
 
+class AntibodyTxtJSON:
+
+    def __init__(self):
+        self.test = None
+
+    def txt_to_dict(self, filename):
+        with open(filename, "r") as f:
+            for line in f.read():
+                print(line)
+
+
+def main():
+    AntibodyJSON = AntibodyTxtJSON
+    AntibodyJSON.txt_to_dict()
