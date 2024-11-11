b7daad5c5e19499f704ff212fcb6c41dd0510682
Sinan Mehmedov <sinanat@abv.bg>
2024-11-11 17:12:26 +0000

    Save Code

--- antibody_to_json.py
+++ antibody_to_json.py
@@ -6,12 +6,23 @@ class AntibodyTxtJSON:
     def __init__(self):
         self.test = None
 
-    def txt_to_dict(self, filename):
+    @staticmethod
+    def txt_to_dict(filename):
+        hanging_key = None
         with open(filename, "r") as f:
-            for line in f.read():
-                print(line)
+            for line in f.readlines():
+                if len(line) > 1:
+                    split_line = line.strip().split(": ", 1)
+                    if len(split_line) == 1:
+                        hanging_key = split_line[0]
+                    print(split_line)
 
 
 def main():
-    AntibodyJSON = AntibodyTxtJSON
-    AntibodyJSON.txt_to_dict()
+    antibody_json = AntibodyTxtJSON()
+    antibody_json.txt_to_dict("11678.txt")
+
+
+if __name__ == "__main__":
+    main()
+
