8715662462a0e3f49ef63f4015a61bcb6efc10e3
Sinan Mehmedov <sinanat@abv.bg>
2024-11-26 01:50:52 +0000

    Save Code

--- antibody_ann_to_json.py
+++ antibody_ann_to_json.py
@@ -1,12 +1,26 @@
 # Populate JSON file with antibody annotation data
 import json
+import os
 
 
 class AntibodyToJSON:
 
     def __init__(self, path):
         self.path = path
+        self.files = os.listdir(path)
+
+    def read_define_records(self, filename):
+        with open(filename, "r") as f:
+            content = [record.strip() for record in f.read().split(";")]
+            # for item in content:
+            #     print(item)
+            print(content)
 
 
 def main():
     antibody_json = AntibodyToJSON("samples")
+    antibody_json.read_define_records(antibody_json.files[0])
+
+
+if __name__ == "__main__":
+    main()
