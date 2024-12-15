66a346be7e5ffb0251bb3facb240d39a4f2b0f84
pySin <sinan_km@yahoo.com>
2024-12-15 21:50:53 +0000

    Save code

--- runner.py
+++ runner.py
@@ -5,14 +5,14 @@ class AntibodyAnnRunner:
 
     def __init__(self, path):
         self.path = path
-        self.aa_run = antibody_ann_to_json.AntibodyToJSON(path)  # 1 comment
+        self.aa_run = antibody_ann_to_json.AntibodyToJSON(path)
 
     def run(self):
         self.aa_run.single_file_transfer(self.path + "/" + self.aa_run.files[3])
 
 
 def main():
-    runner = AntibodyAnnRunner("samples")  # Comment 2
+    runner = AntibodyAnnRunner("samples")
     runner.run()
 
 
